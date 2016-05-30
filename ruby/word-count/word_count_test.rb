0d973ba57bfc28ba1a80ffe51f8f907c4d9a318b
Steve Pentler <stevepentler@gmail.com>
2016-05-29 23:37:30 -0500

    Allows apostrophes for word count regex

--- ruby/word-count/word_count_test.rb
+++ ruby/word-count/word_count_test.rb
@@ -69,7 +69,6 @@ class PhraseTest < Minitest::Test
   end
 
   def test_with_apostrophes
-    skip
     phrase = Phrase.new("First: don't laugh. Then: don't cry.")
     counts = {
       'first' => 1, "don't" => 2, 'laugh' => 1,
