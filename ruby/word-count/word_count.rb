e2358ae6c5479af96caced9015f0f3a2e517e5a6
Steve Pentler <stevepentler@gmail.com>
2016-05-29 23:25:26 -0500

    Passes first three tests of word count

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Phrase
+  attr_accessor :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def word_count
+    final_count = {}
+    split_string.each do |word|
+      if final_count[word]
+        final_count[word] += 1
+      else
+        final_count[word] = 1
+      end
+    end
+    final_count
+  end
+
+  def split_string
+    string.split
+  end
+end
