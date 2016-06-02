47746a2f2ee28f49fb1a1724aecfcace3bcbfcbe
Steve Pentler <stevepentler@gmail.com>
2016-06-02 15:45:22 -0500

    Instantiates empty database

---
+++ ruby/grade-school/grade_school.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class School
+  attr_reader :database
+
+  def initialize
+    @database = Database.new
+  end
+
+  def add(student, grade)
+  end
+
+  def to_h
+    database.db
+  end
+end
+
+class Database
+  attr_reader :db
+
+  def initialize
+    @db = Hash.new { |hash, key| hash[key] = [] }
+  end
+end
